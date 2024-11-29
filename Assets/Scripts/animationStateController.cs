e87f5c6282ed40cf0649afd8f1b35a4d40125e8b
C342 <mbuzurin2024@elam.co.uk>
2024-11-29 12:43:18 +0000

    AnimCode

--- Assets/Scripts/animationStateController.cs
+++ Assets/Scripts/animationStateController.cs
@@ -1,7 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class animationStateController : MonoBehaviour
 {
+    Animator animator;
+    int isWalkingHash;
+    int isJumpingHash;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isJumpingHash = Animator.StringToHash("isJumping");
+    }
+
+    private void Update()
+    {
+        bool isJumping = animator.GetBool(isJumpingHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool isJumping = Input.GetKey("space");
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+        
+        if (!isJumping && (forwardPressed && isJumping))
+        {
+            animator.SetBool(isJumpingHash, true);
+        }
+
+        if (isJumping && (!forwardPressed || !isJumping))
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
